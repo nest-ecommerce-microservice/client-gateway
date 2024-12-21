f8a94d34ec8796975b8203be5084aaaceaf4884f
Walter <walterdcarrizo.19@gmail.com>
2024-12-21 01:03:23 -0300

    initialize NestJS application with basic configuration and environment setup

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(envs.port);
+
+  logger.log(`Server is running on: ${envs.port}`);
+}
+bootstrap();
